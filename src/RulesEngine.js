d765663f3e4bef42bc644b9330617ba41e77b53b
lentan <lentan1029@gmail.com>
2017-07-31 13:16:35 +0800

    Use JSON.stringify instead of deep recursive copy

--- src/RulesEngine.js
+++ src/RulesEngine.js
@@ -63,28 +63,7 @@
   };
 
   RulesEngine.prototype.getFacts = function(accessor) {
-    var recursiveDeepCopy = function(o) {
-    	var newO, i;
-    	if (typeof o !== 'object') return o
-    	if (!o) return o
-
-    	if ('[object Array]' === Object.prototype.toString.apply(o)) {
-    		newO = [];
-    		for (i = 0; i < o.length; i += 1) {
-    			newO[i] = recursiveDeepCopy(o[i]);
-    		}
-    		return newO;
-    	}
-
-    	newO = {};
-    	for (i in o) {
-    		if (o.hasOwnProperty(i)) {
-    			newO[i] = recursiveDeepCopy(o[i]);
-    		}
-    	}
-    	return newO;
-    }
-    var res = recursiveDeepCopy(this.facts);
+    var res = JSON.parse(JSON.stringify(this.facts));
     if (accessor === undefined || typeof accessor !== 'string') return res;
     var arr = accessor.split('.');
     for (var i = 0; i < arr.length; i++) {
